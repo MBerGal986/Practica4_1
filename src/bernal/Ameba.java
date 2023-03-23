944379226693d63bb23cbec4980424648585ea9a
mi <mi@DESKTOP-LGCJ4V3>
2023-03-23 14:23:25 +0100

    Se ha ampliado la salida por pantalla (20/03/2023)

--- src/bernal/Ameba.java
+++ src/bernal/Ameba.java
@@ -19,7 +19,8 @@ public class Ameba {
 	
 	@Override
 	public String toString() {
-		return "Soy una ameba y peso " + peso + " microgramos. ¡Viva!";
+		return "Soy una ameba y peso " + peso + " microgramos. ¡Viva!"
+			 + "\nGracias por utilizar este programa.";
 	}
 
 }
