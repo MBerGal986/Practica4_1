7f4b8cedf760528deb630084984ca20a53674088
mi <mi@DESKTOP-LGCJ4V3>
2023-03-23 13:35:00 +0100

    Se ha modificado la salida por pantalla (20/03/2023)

---
+++ src/bernal/Ameba.java
@@ -0,0 +1,25 @@
+package bernal;
+
+public class Ameba {
+	
+	private int peso;
+	
+	public Ameba () {
+		this.peso = 3;
+	}
+	
+	public void come (int comida) {
+		this.peso += comida - 1;
+	}
+	
+	public void come (Ameba otraAmeba) {
+		this.peso += otraAmeba.peso - 1;
+		otraAmeba.peso = 0;
+	}
+	
+	@Override
+	public String toString() {
+		return "Soy una ameba y peso " + peso + " microgramos.";
+	}
+
+}
